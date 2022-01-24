b8238c898527cb89c3b51818d8ae10faca57e651
������ <bingfengdev@aliyun.com>
2022-01-24 11:28:52 +0800

    feat: 定义cache接口，屏蔽缓存实现细节

--- src/gt/api/v1/ping.go
+++ src/gt/api/v1/ping.go
@@ -2,8 +2,11 @@ package v1
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/bingfenglai/gt/config"
+	"github.com/bingfenglai/gt/global"
+	"github.com/bingfenglai/gt/models/cache"
 	"go.uber.org/zap"
 
 	"github.com/gin-gonic/gin"
@@ -17,6 +20,10 @@ func Ping(c *gin.Context) {
 	p := config.Conf.Server
 
 	zap.L().Info("健康检查接口调用")
+
+	cache := cache.NewRedisCache(global.RedisClient,time.Minute*30)
+
+	cache.SetWithDefaultExpiration(c.Request.Host,c.Request.UserAgent())
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
