3d5c593fc3a27d5a211b7ea4b7aa07c1092f4697
������ <bingfengdev@aliyun.com>
2022-06-09 17:44:31 +0800

    fix

--- backend/domain/params/captcha_verity.go
+++ backend/domain/params/captcha_verity.go
@@ -1,6 +1,6 @@
 package params
 
-// 验证码校验参数
+// VerityCaptchaParams 验证码校验参数
 type VerityCaptchaParams struct {
 	Dots      string `json:"dots" binding:"required"`
 	CaptchaId string `json:"captcha_id" binding:"required"`
